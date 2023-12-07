8b69da4bbf84c254070319748f5ee02c04f877b2
billy clark <billy_clark@presciencelabs.org>
2023-12-07 13:10:34 -0500

    users requested level colors match TBTA

--- app/src/lib/Level.svelte
+++ app/src/lib/Level.svelte
@@ -3,6 +3,44 @@
 	export let level
 </script>
 
-<span class="badge badge-primary badge-lg">
+<span class={`badge L${level} badge-lg`}>
 	L{level}
 </span>
+
+<style>
+/*
+	level colors in TBTA are as follows:
+		0 => blue
+		1 => creme
+		2 => magenta
+		3 => green
+		4 => brown
+*/
+
+	.L0 {
+		color: whitesmoke;
+		background-color: #0B66FF;
+		border-color: whitesmoke;
+	}
+	.L1 {
+		color: darkblue;
+		background-color: #FCFFC5;
+		filter: saturate(200%);
+		border-color: darkblue;
+	}
+	.L2 {
+		color: yellow;
+		background-color: #FB00FF;
+		border-color: yellow;
+	}
+	.L3 {
+		color: whitesmoke;
+		background-color: #0F7000;
+		border-color: whitesmoke;
+	}
+	.L4 {
+		color: whitesmoke;
+		background-color: #6B2F30;
+		border-color: whitesmoke;
+	}
+</style>
