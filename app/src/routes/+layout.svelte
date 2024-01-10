5a1ee1fab707ac2c9d1017f249a5cf94b436cf51
billy clark <billy_clark@presciencelabs.org>
2024-01-10 16:48:11 -0500

    added link to new tool

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import '$lib/app.postcss'
 	import {Selector} from '$lib/theme'
 
-	/** @type {import('./$types').PageData} */
 	export let data
 </script>
 
@@ -11,11 +10,19 @@
 <slot />
 
 <!-- https://daisyui.com/components/footer -->
-<footer class="prose footer mt-20 max-w-none bg-secondary p-10 text-secondary-content">
+<footer class="prose footer mt-20 max-w-none bg-neutral p-10 text-neutral-content">
 	<nav>
 		<header class="footer-title">Theme</header>
 
 		<Selector colors="bg-accent text-accent-content" />
+	</nav>
+
+	<nav>
+		<header class="footer-title">Tools</header>
+
+		<a href="/p1-to-eng">
+			Back translate Phase 1 to ENG
+		</a>
 	</nav>
 
 	<nav class="place-self-start md:justify-self-end">
@@ -27,8 +34,8 @@
 			<span data-tip="TBD: still need to integrate auth" class="badge indicator-item badge-warning badge-xs tooltip tooltip-left" />
 		</a> -->
 
-		<a href="//alltheword.org/downloads-software-bible" target="_blank">
-			<pre>{data.version}</pre>
+		<a href="//alltheword.org/downloads-software-bible" target="_blank" class="font-mono">
+			{data.version}
 		</a>
 	</nav>
 </footer>
