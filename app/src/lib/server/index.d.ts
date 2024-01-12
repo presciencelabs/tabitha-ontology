8633d068cef1e820c243af1df57ae885844534cf
Craig Palmer <37323396+cpalmer19@users.noreply.github.com>
2024-01-12 12:28:49 -0800

    Add simple phase 2 rendering in the example occurrences

--- app/src/lib/server/index.d.ts
+++ app/src/lib/server/index.d.ts
@@ -49,3 +49,21 @@ type Phrase = {
 	role: string
 	word: string
 }
+
+type SourceData = {
+	type: string
+	id_primary: string
+	id_secondary: string
+	id_tertiary: string
+	phase_1_encoding: string
+	phase_2_encoding: string
+	comments: string
+	notes: string
+}
+
+type SourceEntity = {
+	label: string
+    features: string
+	sense: string
+    entity: string
+}
