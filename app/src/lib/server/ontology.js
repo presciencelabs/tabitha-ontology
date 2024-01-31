6e08e41218633fd40639fbb450ca45998fe0259e
billy clark <billy_clark@presciencelabs.org>
2024-01-31 12:32:55 -0500

    added ability to search with a sense

--- app/src/lib/server/ontology.js
+++ app/src/lib/server/ontology.js
@@ -1,40 +1,81 @@
 import {transform} from './transformers'
 
+// refs:
+// 	https://www.sqlite.org/lang_expr.html#the_like_glob_regexp_match_and_extract_operators
+// 	https://developers.cloudflare.com/d1/platform/client-api/#searching-with-like
+// 	https://developers.cloudflare.com/d1/platform/client-api/#parameter-binding
+
 /**
- * case-insensitive match, will accept % as a wildcard
+ * case-insensitive match, will accept % as a wildcard as well as sense-specific search, e.g., love-A
  *
  * @param {import('@cloudflare/workers-types').D1Database} db
  *
  * @returns {(filter: string) => Promise<Concept[]>}
  */
 export const get_concepts = db => async filter => {
-	// https://www.sqlite.org/lang_expr.html#the_like_glob_regexp_match_and_extract_operators
-	// https://developers.cloudflare.com/d1/platform/client-api/#searching-with-like
-	// https://developers.cloudflare.com/d1/platform/client-api/#parameter-binding
-	const sql = `
-		SELECT *
-		FROM Concepts
-		WHERE stem like ?
-	`
+	// senses follow the form word-A, /^(.*)-([A-Z])$/
+	const matches = filter.match(/^(.*)-([A-Z])$/)
+	if (matches) {
+		const [, stem, sense] = matches
+		return await by_sense({stem, sense})
+	}
 
-	/** @type {import('@cloudflare/workers-types').D1Result<DbRowConcept>} https://developers.cloudflare.com/d1/platform/client-api/#return-object */
-	const {results} = await db.prepare(sql).bind(normalize_wildcards(filter)).all()
+	return await by_filter(filter)
 
-	return normalize(results)
-}
+	/**
+	 * @param {{stem: string, sense: string}} input
+	 * @returns {Promise<Concept[]>}
+	 */
+	async function by_sense({stem, sense}) {
+		const sql = `
+			SELECT *
+			FROM Concepts
+			WHERE stem = ?
+			AND sense = ?
+		`
 
-/**
- * @param {DbRowConcept[]} matches_from_db
- *
- * @returns {Concept[]}
- */
-function normalize(matches_from_db) {
-	return matches_from_db.map(transform)
+		/** @type {import('@cloudflare/workers-types').D1Result<DbRowConcept>} https://developers.cloudflare.com/d1/platform/client-api/#return-object */
+		const {results} = await db.prepare(sql).bind(stem, sense).all() // note: love-A will still return 2 results, the noun and the verb
+
+		return normalize(results)
+	}
+
+	/**
+	 * @param {string} filter
+	 * @returns {Promise<Concept[]>}
+	 */
+	async function by_filter(filter) {
+		const sql = `
+			SELECT *
+			FROM Concepts
+			WHERE stem like ?
+		`
+
+		/** @type {import('@cloudflare/workers-types').D1Result<DbRowConcept>} */
+		const {results} = await db.prepare(sql).bind(normalize_wildcards(filter)).all()
+
+		return normalize(results)
+	}
+
+	/**
+	 * @param {string} possible_wildard – a string that may contain wildcards, e.g., '*' or '#' or '%'
+	 * @returns {string} SQL-ready string, i.e., `%` for wildcards
+	 */
+	function normalize_wildcards(possible_wildard) {
+		return possible_wildard.replace(/[*#]/g, '%')
+	}
+
+	/**
+	 * @param {DbRowConcept[]} matches_from_db
+	 * @returns {Concept[]}
+	 */
+	function normalize(matches_from_db) {
+		return matches_from_db.map(transform)
+	}
 }
 
 /**
  * @param {import('@cloudflare/workers-types').D1Database} db
- *
  * @returns {Promise<string>}
  */
 export async function get_version(db) {
@@ -46,13 +87,4 @@ export async function get_version(db) {
 	// prettier-ignore
 	/** @type {string} https://developers.cloudflare.com/d1/platform/client-api/#await-stmtfirstcolumn */
 	return await db.prepare(sql).first('version') || ''
-}
-
-/**
- * @param {string} possible_wildard – a string that may contain wildcards, e.g., '*' or '#' or '%'
- *
- * @returns {string} SQL-ready string, i.e., `%` for wildcards
- */
-function normalize_wildcards(possible_wildard) {
-	return possible_wildard.replace(/[*#]/g, '%')
 }
