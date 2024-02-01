fe646a8e8322963413818b367c8ab12eab39bf61
billy clark <billy_clark@presciencelabs.org>
2024-02-01 10:02:31 -0500

    removed Stephen's backtranslator since it moved to the editor

--- app/src/routes/(app)/+layout.svelte
+++ app/src/routes/(app)/+layout.svelte
@@ -3,19 +3,12 @@
 </script>
 
 <!-- layout not handled by daisyUI, https://daisyui.com/docs/layout-and-typography -->
-<header class="grid grid-cols-[auto_1fr]">
+<header class="grid grid-cols-[auto_1fr] mx-8 mt-8">
 	<Brand />
 
 	<Search autofocus />
 </header>
 
-<main>
+<main class="mx-8 mt-8">
 	<slot />
 </main>
-
-<style lang="postcss">
-	header,
-	main {
-		@apply mx-8 mt-8;
-	}
-</style>
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -17,31 +17,12 @@
 		<Selector colors="bg-accent text-accent-content" />
 	</nav>
 
-	<nav>
-		<header class="footer-title">Tools</header>
-
-		<a href="/p1-to-eng">
-			Back translate Phase 1 to ENG
-		</a>
-	</nav>
-
 	<nav class="place-self-start md:justify-self-end">
 		<header class="footer-title">Administer</header>
 
 		<!-- https://daisyui.com/components/link -->
-		<!-- <a href="//btn.social" target="_blank" class="indicator">
-			Update data
-			<span data-tip="TBD: still need to integrate auth" class="badge indicator-item badge-warning badge-xs tooltip tooltip-left" />
-		</a> -->
-
-		<a href="//alltheword.org/downloads-software-bible" target="_blank" class="font-mono">
+		<a href="//alltheword.org/downloads-software-bible" target="_blank" class="font-mono link link-hover">
 			{data.version}
 		</a>
 	</nav>
 </footer>
-
-<style lang="postcss">
-	a {
-		@apply link-hover link;
-	}
-</style>
