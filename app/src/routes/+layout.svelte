3c1aa66dbdc07e9e15dd1aa5ed17fde6127df2a8
billy clark <billy_clark@presciencelabs.org>
2024-03-19 09:39:29 -0400

    no need for (app) subfolder anymore.

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '$lib/app.postcss'
+	import { Brand, Search } from '$lib'
 	import { Selector } from '$lib/theme'
 
 	export let data
@@ -7,7 +8,15 @@
 
 <!-- layout not handled by daisyUI, https://daisyui.com/docs/layout-and-typography -->
 
-<slot />
+<header class="grid grid-cols-[auto_1fr] mx-8 mt-8">
+	<Brand />
+
+	<Search autofocus />
+</header>
+
+<main class="mx-8 mt-8">
+	<slot />
+</main>
 
 <!-- https://daisyui.com/components/footer -->
 <footer class="prose footer mt-20 max-w-none bg-neutral p-10 text-neutral-content">
