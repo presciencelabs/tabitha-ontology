ef8b6020412ca4496ce33c338cb89fd9069d8188
craigp-atw <152427351+craigp-atw@users.noreply.github.com>
2024-04-02 19:11:24 -0700

    Add cache header to api responses (#29)

--- app/src/routes/+server.js
+++ app/src/routes/+server.js
@@ -9,12 +9,23 @@ export async function GET({ url: { searchParams }, locals: { db } }) {
 
 	const lite_matches = matches.map(make_lite)
 
-	return json(lite_matches)
+	return response(lite_matches)
 
 	/** @param {Concept} concept */
 	function make_lite(concept) {
 		const { id, stem, sense, part_of_speech, level, gloss, categorization } = concept
 
 		return { id, stem, sense, part_of_speech, level, gloss, categorization }
+	}
+
+	/** @param {any} result  */
+	function response(result) {
+		const THREE_HOUR_CACHE = {
+			'cache-control': `max-age=${3 * 60 * 60}`,
+		}
+
+		return json(result, {
+			headers: THREE_HOUR_CACHE,
+		})
 	}
 }
