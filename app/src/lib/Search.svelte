9d6a46a5625a20e280ae221686b3c912e42447e2
Craig Palmer <37323396+cpalmer19@users.noreply.github.com>
2024-07-05 11:41:37 -0700

    Use dropdown instead

--- app/src/lib/Search.svelte
+++ app/src/lib/Search.svelte
@@ -9,8 +9,8 @@
 	/** @type {string|null} */
 	let value = new URLSearchParams($page.url.search).get('q')
 
-	/** @type {string|null} */
-	let category = new URLSearchParams($page.url.search).get('category')
+	/** @type {string} */
+	let category = new URLSearchParams($page.url.search).get('category') || 'all'
 	const categories = ['Noun', 'Verb', 'Adjective', 'Adverb', 'Adposition', 'Conjunction', 'Particle', 'Phrasal']
 
 	let scope = writable('')
@@ -38,39 +38,27 @@
 <search>
 	<!--
 		used role="search" ∵ https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/search#search_form_labels_and_accessibility.
-
-		decided against daisy's "join" approach in favor of positioning so I could get a focus ring around the input AND button.  See https://github.com/saadeghi/daisyui/discussions/2400>
 	-->
-	<form role="search">
-		<fieldset class="join gap-0 text-center w-full">
-			<label class="btn join-item" class:btn-accent={!category || category === 'all'}>
-				All
-				<input type="radio" name="category" bind:group={category} value="all" class="hidden" />
-			</label>
+	<form role="search" class="join w-full">
+		<input type="search" name="q" id="q" bind:value use:auto_focus class="input input-bordered input-primary input-lg w-full join-item" />
+
+		<select name="category" bind:value={category} class="select select-primary select-lg join-item">
+			<option value="all">All Concepts</option>
 			{#each categories as category_value}
-				<label class="btn join-item" class:btn-accent={category === category_value}>
-					{category_value}s
-					<input type="radio" name="category" bind:group={category} value="{category_value}" class="hidden" />
-				</label>
+				<option value="{category_value}">{category_value}s</option>
 			{/each}
-		</fieldset>
+		</select>
 
-		<br />
+		<select name="scope" bind:value={$scope} on:change={store_scope} class="select select-primary select-lg join-item">
+			<option value="stems">Stems only</option>
+			<option value="glosses">Glosses only</option>
+			<option value="all">Stems and Glosses</option>
+		</select>
 
-		<div class="join w-full">
-			<input type="search" name="q" id="q" bind:value use:auto_focus class="input input-bordered input-primary input-lg w-full join-item" />
-	
-			<select name="scope" bind:value={$scope} on:change={store_scope} class="select select-primary select-lg join-item">
-				<option value="stems">Stems only</option>
-				<option value="glosses">Glosses only</option>
-				<option value="all">Stems and Glosses</option>
-			</select>
-	
-			<button class="btn btn-primary btn-lg join-item">
-				<span class="hidden sm:inline">Search</span>
-				<Icon icon="material-symbols:search" class="h-6 w-6" />
-			</button>
-		</div>
+		<button class="btn btn-primary btn-lg join-item">
+			<span class="hidden sm:inline">Search</span>
+			<Icon icon="material-symbols:search" class="h-6 w-6" />
+		</button>
 	</form>
 
 	<!-- chose visibility here to keep results from jumping up and down -->
