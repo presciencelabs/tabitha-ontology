d4fa96163709f586ba97f53dedabe53f91fbeede
billy clark <billy.clark@presciencelabs.org>
2024-10-19 15:21:56 -0400

    docs were not accurate anymore

--- src/lib/examples/examples.js
+++ src/lib/examples/examples.js
@@ -1,15 +1,4 @@
-/**
- * Context arguments come after the reference part of an example.
- * eg. for a Verb: 4|1|20|13|p|A|SarahA|AbrahamA||||||| -> p|A|SarahA|AbrahamA|||||||
- * eg. for a Noun: 4|19|23|6|followA|A -> followA|A
- *
- * Each slot (separated by '|') is empty or present based on the word's usage in the example.
- * Each part of speech has a unique, fixed number of 'slots'. For example, Nouns are simple with
- * just two slots indicating the Verb the Noun is attached to, and the semantic role it
- * plays in the clause.
- *
- * @type { import('.').ContextArgumentMap }
- */
+/** @type { import('.').ContextArgumentMap } */
 export const context_argument_map = new Map([
 	['Noun', [
 		'Verb',
