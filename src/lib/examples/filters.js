66a3b11b0dc48289ca1dff4cee063fa7bbaf1e92
billy clark <billy.clark@presciencelabs.org>
2024-10-22 12:30:35 -0400

    had a singleton bug that need to be refactored

--- src/lib/examples/filters.js
+++ src/lib/examples/filters.js
@@ -1,30 +1,6 @@
 import { bible_books } from '$lib/lookups'
 import { context_argument_map } from './examples'
 
-/** @type { import('.').FilterMap } */
-const filter_map = initialize()
-
-function initialize() {
-	/** @type { import('.').FilterMap } */
-	const initialized_filter_map = new Map()
-
-	for (const [part_of_speech, args] of argument_slots.entries()) {
-		/** @type { import('.').Filters } */
-		const initialized_filters = new Map()
-
-		for (const filter_name of args) {
-			/** @type { import('.').Options } */
-			const options = new Set()
-
-			initialized_filters.set(filter_name, options)
-		}
-
-		initialized_filter_map.set(part_of_speech, initialized_filters)
-	}
-
-	return initialized_filter_map
-}
-
 /**
  * Example data:
  * ______________________________________________
@@ -42,24 +18,72 @@ function initialize() {
  * | Adposition	| ...									|
  * | Outer Nouns	| ...									|
  *
+ *    ============= sleep-A ================
+ * | Topic NP		| 'Most Agent-Like'				|
+ * | Polarity		| 'Affirmative', 'Negative'	|
+ * | Agent			| ...									|
+ * | Addressee		| ...									|
+ *
+ *    ============= speak-A ================
+ * | Topic NP		| ...				|
+ * | Polarity		| 'Affirmative', 'Negative'	|
+ * | Agent			| ...									|
+ * | Patient		| ...									|
+ * | ...				| ...									|
+ *
+ *    ============= speak-B ================
+ * | Topic NP		| 'Most Agent-Like'				|
+ * | Polarity		| 'Affirmative'					|
+ * | Agent			| ...									|
+ * | Patient		| ...									|
+ *
  * @param { Concept } concept
  * @param { Example[] } examples
  * @returns { import('.').Filters }
  */
-export function derive_filters({ part_of_speech }, examples) {
+export function derive_filters(concept, examples) {
 	/** @type { import('.').Filters } */
 	const filters = new Map()
 
-	/** @type { import('.').Option[] } */
-	const common_options = ['Any']
-
+	// The Book filter has to be handled separately because it's a little different than the context filters.
 	const book_names_found_in_examples = examples.sort(by_book_order).map(book_name)
-	filters.set('Book', new Set([...common_options, ...book_names_found_in_examples]))
+	filters.set('Book', new Set(['Any', ...book_names_found_in_examples]))
 
-	for (const [name, options] of derive_context_filters(part_of_speech).entries().filter(has_options)) {
+	/** @type { import('.').Filters } */
+	const context_filters = initialize_filter_map().get(concept.part_of_speech) ?? new Map()
+
+	/**
+	 * @typedef { number } Counter
+	 * @type { Record<ContextArgumentName, Counter> }
+	 */
+	const absence_tracker = {}
+
+	examples.forEach(({ context }) => {
+		context_filters.forEach((options, argument_name) => {
+			if (context[argument_name]) {
+				context_filters.get(argument_name)?.add(context[argument_name])
+			}
+			else {
+				absence_tracker[argument_name] = (absence_tracker[argument_name] ?? 0) + 1
+			}
+		})
+	})
+
+	for (const [name, options] of context_filters.entries().filter(has_options)) {
+		const common_options = new Set()
+
+		if (options.size > 1) {
+			common_options.add('Any')
+		}
+
+		if (absence_tracker[name]) {
+			common_options.add('Present')
+			common_options.add('Not present')
+		}
+
 		const sorted_options = [...options].sort()
 
-		filters.set(name, new Set([...common_options,...presence_options(name), ...sorted_options]))
+		filters.set(name, new Set([...common_options, ...sorted_options]))
 	}
 
 	return filters
@@ -69,39 +93,30 @@ export function derive_filters({ part_of_speech }, examples) {
 		return id_primary
 	}
 
-	/**
-	 * @param { Concept['part_of_speech'] } part_of_speech
-	 * @returns { import('.').Filters }
-	 */
-	function derive_context_filters(part_of_speech) {
-		/** @type { import('.').Filters } */
-		const context_filters = filter_map.get(part_of_speech) ?? new Map()
+	function initialize_filter_map() {
+		/** @type { import('.').FilterMap } */
+		const filter_map = new Map()
 
-		examples.forEach(({ context }) => {
-			for (const argument_name of Object.keys(context)) {
-				context_filters.get(argument_name)?.add(context[argument_name])
+		for (const [part_of_speech, args] of context_argument_map.entries()) {
+			/** @type { import('.').Filters } */
+			const filters = new Map()
+
+			for (const filter_name of args) {
+				/** @type { import('.').Options } */
+				const options = new Set()
+
+				filters.set(filter_name, options)
 			}
-		})
 
-		return context_filters
+			filter_map.set(part_of_speech, filters)
+		}
+
+		return filter_map
 	}
 
 	/** @param { [ContextArgumentName, import('.').Options] } filters  */
 	function has_options([, options]) {
 		return options.size > 0
-	}
-
-	/**
-	 * @param { ContextArgumentName } name
-	 * @returns { Array<ContextArgumentName> }
-	 */
-	function presence_options(name) {
-		return presence_option_is_applicable(name) ? ['Present', 'Not present'] : []
-
-		/** @param { ContextArgumentName } name */
-		function presence_option_is_applicable(name) {
-			return !['Role', 'Topic NP', 'Polarity', 'Agent', 'Patient'].includes(name)
-		}
 	}
 }
 
