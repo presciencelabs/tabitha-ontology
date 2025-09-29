eeca4b83a3c49c2f7f68048cdc0d8837c1c9c264
billy clark <billy.clark@presciencelabs.org>
2025-09-29 14:17:30 -0400

    new download capability

---
+++ src/routes/downloads/+page.server.ts
@@ -0,0 +1,50 @@
+import { S3Client, ListObjectsV2Command, type _Object } from '@aws-sdk/client-s3'
+import type { PageServerLoad } from './$types'
+import { CLOUDFLARE_ACCOUNT_ID, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY } from '$env/static/private'
+
+type Backup = {
+	name: string,
+	size_mb: number,
+	created_at: Date,
+	url: string,
+}
+
+export const load: PageServerLoad = async () => {
+	console.info('checking for downloads...')
+
+	const S3 = new S3Client({
+		region: 'auto',
+		endpoint: `https://${CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+		credentials: {
+			accessKeyId: S3_ACCESS_KEY_ID,
+			secretAccessKey: S3_SECRET_ACCESS_KEY,
+		},
+	})
+
+	const objects = await S3.send(new ListObjectsV2Command({
+		Bucket: 'db-backups'
+	}))
+
+	console.info(`found ${objects.KeyCount} objects`)
+
+	const backups = objects.Contents?.map(obj => {
+		return {
+			name: obj.Key!,
+			size_mb:  bytes_to_mb(obj.Size!),
+			created_at: new Date(obj.LastModified!),
+			url: `https://db-backups.tabitha.bible/${obj.Key}`
+		}
+	}).sort(most_recent_first) ?? []
+
+	return {
+		backups: backups,
+	}
+
+	function bytes_to_mb(bytes: number) {
+		return Math.round(bytes / (1024 * 1024))
+	}
+
+	function most_recent_first(a: Backup, b: Backup) {
+		return b.created_at.getTime() - a.created_at.getTime()
+	}
+}
---
+++ src/routes/downloads/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte'
+	import type { PageProps } from './$types'
+
+	let { data }: PageProps = $props()
+</script>
+
+<table class="table">
+	<thead>
+		<tr>
+			<th>Database</th>
+			<th>Created</th>
+			<th>Size</th>
+			<th></th>
+		</tr>
+	</thead>
+
+	<tbody>
+		{#each data.backups as { name, created_at, size_mb, url } (name)}
+			<tr class="hover:bg-base-300">
+				<td>{name}</td>
+				<td>{created_at.toLocaleString()}</td>
+				<td>{size_mb} MB</td>
+				<td>
+					<a href={url} download class="btn btn-sm btn-secondary">
+						<Icon icon="material-symbols:download-rounded" class="h-5 w-5"/>
+					</a>
+				</td>
+			</tr>
+		{:else}
+			<tr><td colspan="4">No backups available.</td></tr>
+		{/each}
+	</tbody>
+</table>
