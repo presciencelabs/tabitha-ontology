944b18278e21b93db4315408053903b1ae1036d1
craigp_atw <craig_palmer@alltheword.org>
2025-11-04 14:03:13 -0800

    Add option to search english text

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,6 @@
 import { get_concepts } from '$lib/server/ontology'
 import { get_function_words } from '$lib/server/function_words'
+import { redirect } from '@sveltejs/kit'
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ url: { searchParams }, locals: { db_ontology } }) {
@@ -9,6 +10,10 @@ export async function load({ url: { searchParams }, locals: { db_ontology } }) {
 		scope: 'stems',
 		category: '',
 		...Object.fromEntries(searchParams),
+	}
+
+	if (search_filter.scope === 'english') {
+		throw redirect(303, `https://targets.tabitha.bible/English/search?q=${search_filter.q}`)
 	}
 
 	const results = [
