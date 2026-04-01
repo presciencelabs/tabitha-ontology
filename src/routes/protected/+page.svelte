915200bf8fea9b7a3dede85bff0fbbcba1918d70
craigp_atw <craig_palmer@alltheword.org>
2026-04-01 14:19:42 -0700

    Add 'add concept' buttons

--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -1,1 +1,17 @@
-Ontology CRUD landing page...
+<script>
+	import Icon from '@iconify/svelte'
+	let { data } = $props()
+</script>
+
+<section class="mb-5">
+	Welcome to the Ontology Management page. More features coming soon!
+</section>
+
+{#if data.can_add}
+	<section>
+		<a class="btn" href="/protected/concept/create">
+			<Icon icon="material-symbols:add" class="w-5 h-5" />
+			Add Concept
+		</a>
+	</section>
+{/if}
