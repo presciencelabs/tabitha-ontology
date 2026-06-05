fbb2fc2277daeece78e2d1005dd87dd456ab04e3
craigp-atw <152427351+craigp-atw@users.noreply.github.com>
2026-06-05 12:50:54 -0700

    semantic search cache and ui changes (#95)

--- src/lib/server/semantic_search.ts
+++ src/lib/server/semantic_search.ts
@@ -6,13 +6,21 @@ import type { D1Database } from '@cloudflare/workers-types'
 export async function find_related_concepts(db: D1Database, search_term: string): Promise<Concept[]> {
 	const all_concepts = await get_all_concepts(db)
 
+	// check the cache
+	const cached_results = get_from_cache(search_term)
+	if (cached_results) {
+		return cached_results.map(key => all_concepts.find(c => key === concept_key(c))!)
+	}
+
 	// These filters currently result in ~3800 concepts getting sent to the LLM, down from ~6380
 	const concept_filters: ((c: Concept) => boolean)[] = [
 		// don't bother including whole numbers, they just use up tokens
 		// leave decimal numbers though, so things like 'tenth' can relate to '.1'
 		c => c.gloss.includes('number') && !!c.stem.match(/^\d/),
 		// don't bother including proper names, unless one of the geographical ones like mount-Horeb, city-David, etc. Unsure about this one
-		c => c.gloss.startsWith('(proper name)') && !c.stem.match(/^(?:sea-|mount-|valley-|river-|desert-|city-|cave-|feast-|gate-)/i),
+		c => c.gloss.startsWith('(proper name)') /*&& !c.stem.match(/^(?:sea-|mount-|valley-|river-|desert-|city-|cave-|feast-|gate-)/i)*/,
+		// don't include dates and times other than '12PM' so it can relate to 'noon'
+		c => !!c.stem.match(/\d(?:BC|AD|PM|AM)$/) && c.stem !== '12PM',
 		// don't include concepts that are going to be deleted
 		c => c.gloss.includes('DELETE'),
 		// don't include the concepts that are exactly the search term (handled separately and would be redundant)
@@ -21,6 +29,7 @@ export async function find_related_concepts(db: D1Database, search_term: string)
 		c => c.stem === search_term,
 	]
 
+	// Note that currently the input is about 73800 tokens, and sometimes triggers 20000-50000 tokens of implicit cache
 	const input_data = {
 		concepts: all_concepts.filter(c => !concept_filters.some(f => f(c))).map(transform_concept),
 		search_term,
@@ -57,6 +66,10 @@ export async function find_related_concepts(db: D1Database, search_term: string)
 	})
 
 	const output = response.text?.length ? JSON.parse(response.text) as string[] : []
+	if (output.length) {
+		set_cache(search_term, output)
+	}
+
 	return output.map(key => all_concepts.find(c => key === concept_key(c))!)
 }
 
@@ -80,4 +93,27 @@ function transform_concept(concept: Concept): { concept: string, gloss: string }
 
 function concept_key({ stem, sense, part_of_speech }: Concept): string {
 	return `${stem}-${sense}-${part_of_speech}`
+}
+
+const related_concept_cache: Map<string, [string[], Date]> = new Map()
+
+function get_from_cache(search_term: string): string[] | undefined {
+	const cached_value = related_concept_cache.get(search_term)
+	if (!cached_value) {
+		return undefined
+	}
+
+	const [results, timestamp] = cached_value
+
+	const a_week_ago = new Date()
+	a_week_ago.setDate(a_week_ago.getDate() - 7)
+	if (timestamp < a_week_ago) {
+		return undefined
+	}
+
+	return [...results]
+}
+
+function set_cache(search_term: string, results: string[]) {
+	related_concept_cache.set(search_term, [[...results], new Date()])
 }
