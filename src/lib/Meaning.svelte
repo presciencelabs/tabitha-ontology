acf7fe087f4259321928a639904aa9a2553d1e77
billy clark <billy.clark@presciencelabs.org>
2026-08-07 21:58:57 -0400

    wanted to follow daisy standards a bit closer
    full width alert was no good in table view

--- src/lib/Meaning.svelte
+++ src/lib/Meaning.svelte
@@ -3,18 +3,35 @@
 
 	/** @type {Concept} */
 	export let concept
+
+	/** @type {boolean} */
+	export let compact = false
 </script>
 
 {#if concept.status === 'not used'}
-	<div role="alert" class="alert alert-error">
-		<Icon icon="mdi:error-outline" class="h-6 w-6" />
-		<span class="font-bold">{concept.gloss}</span>
-	</div>
+	{#if compact}
+		<span class="badge badge-error badge-soft gap-1 font-semibold">
+			<Icon icon="mdi:error-outline" class="h-4 w-4" />
+			{concept.gloss}
+		</span>
+	{:else}
+		<div role="alert" class="alert alert-error alert-soft">
+			<Icon icon="mdi:error-outline" class="h-6 w-6" />
+			<span class="font-bold">{concept.gloss}</span>
+		</div>
+	{/if}
 {:else if concept.status === 'suggested'}
-	<div role="alert" class="alert alert-warning">
-		<Icon icon="mdi:alert-outline" class="h-6 w-6" />
-		<span class="font-bold">{concept.gloss}</span>
-	</div>
+	{#if compact}
+		<span class="badge badge-warning badge-soft gap-1 font-semibold">
+			<Icon icon="mdi:alert-outline" class="h-4 w-4" />
+			{concept.gloss}
+		</span>
+	{:else}
+		<div role="alert" class="alert alert-warning alert-soft">
+			<Icon icon="mdi:alert-outline" class="h-6 w-6" />
+			<span class="font-bold">{concept.gloss}</span>
+		</div>
+	{/if}
 {:else}
 	{concept.gloss}
 {/if}
