9008cf4ddbc481df59f7eccf0b7d450f9fe8f64c
craigp_atw <craig_palmer@alltheword.org>
2026-08-13 19:14:04 -0700

    Use clock icon instead of ...

--- src/lib/PendingChange.svelte
+++ src/lib/PendingChange.svelte
@@ -4,7 +4,7 @@
 
 <div class="border border-info bg-info/5 text-info text-sm p-1 rounded-md flex flex-row items-center gap-1">
 	<span class="tooltip" data-tip="Pending change...">
-		<Icon icon="material-symbols:pending-outline" class="h-4 w-4" />
+		<Icon icon="mdi:clock-outline" class="h-4 w-4" />
 	</span>
 	<slot />
 </div>
