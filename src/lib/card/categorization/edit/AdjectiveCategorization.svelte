6679909d53edcbd4583f3b0545134a9159f7e577
billy clark <billy.clark@presciencelabs.org>
2026-08-17 18:37:23 -0400

    PR feedback

--- src/lib/card/categorization/edit/AdjectiveCategorization.svelte
+++ src/lib/card/categorization/edit/AdjectiveCategorization.svelte
@@ -4,11 +4,10 @@
 
 	interface Props {
 		categories?: string[]
-		part_of_speech?: string
 	}
 
-	let { categories = $bindable([]), part_of_speech = 'Adjective' }: Props = $props()
+	let { categories = $bindable([]) }: Props = $props()
 </script>
 
-<SemanticCategorization {part_of_speech} bind:categories={categories} />
-<UsageInfo {part_of_speech} bind:categories={categories} offset={1} />
+<SemanticCategorization part_of_speech="Adjective" bind:categories={categories} />
+<UsageInfo part_of_speech="Adjective" bind:categories={categories} offset={1} />
